e1070d89e6ee14052fba709b6398fa04abd4b2e6
Charles-CarM <103493003+Charles-CarM@users.noreply.github.com>
2022-08-23 07:28:16 +0800

    connected

--- src/App.js
+++ src/App.js
@@ -3,18 +3,21 @@ import axios from 'axios'
 import './App.css';
 import Header from './components/ui/Header'
 import CharacterGrid from './components/characters/CharacterGrid'
+import Pagination from './components/pagination/Pagination'
+import Search from './components/ui/Search'
 
 
 const App = () => {
   //const [items, setItems] = useState([])
   const [items, setItems] = useState([])
   const [isLoading, setIsLoading] = useState(true) 
+  const [query, setQuery] = useState('') 
   const [currentPage, setCurrentPage] = useState(1)
-  const [postsPerPage, setPostsPerPage] = useState(8)
+  const [itemsPerPage] = useState(8)
 
   useEffect(() => {
     const getItems = async () => {
-      const result = await axios(`https://www.breakingbadapi.com/api/characters`)
+      const result = await axios(`https://www.breakingbadapi.com/api/characters?name=${query}`)
 
       setItems(result.data)
       setIsLoading(false)
@@ -23,17 +26,27 @@ const App = () => {
   }, [])
 
   //Get current Items
-  const indexOfLastItem = currentPage * postsPerPage
-  const indexOfFirstItem = indexOfLastItem - postsPerPage
+  const indexOfLastItem = currentPage * itemsPerPage
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage
   const currentItems = items.slice(indexOfFirstItem, indexOfLastItem)
-  console.log(currentItems)
+
+  //change page 
+  const paginate = (pageNumber) => setCurrentPage(pageNumber)
   
   return (
     <div className="container">
       <Header />
+      <Search />
       <CharacterGrid isLoading={isLoading} items={currentItems} />
+      <Pagination 
+        itemsPerPage={itemsPerPage} 
+        totalItems={items.length}
+        paginate={paginate} 
+      />
     </div>
   );
 }
 
 export default App;
+
+//super cool I was able to implement this on the Breaking Bad API project
